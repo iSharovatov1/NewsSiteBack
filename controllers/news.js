d2b7cfaffa46db2a8d0de76a57f5aba26606558c
iSharovatov1 <i.sharovatov@dunice.net>
2022-07-18 15:53:57 +0300

    added endpoint getAllNews

---
+++ controllers/news.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const { News } = require('../db/models');
+
+async function getAllNews(req, res) {
+  try {
+    const news = await News.findAll({
+      order: [
+        ['createdAt', 'DESC'],
+      ],
+    });
+    return res.status(200).send(news);
+  } catch (error) {
+    return res.status(404).send(error);
+  }
+}
+
+module.exports = { getAllNews };
