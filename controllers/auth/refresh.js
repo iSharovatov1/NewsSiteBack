ed67576cf122825a2bd21e769770948ae05700c9
iSharovatov1 <i.sharovatov@dunice.net>
2022-07-29 09:39:59 +0300

    added token matching by userId

--- controllers/auth/refresh.js
+++ controllers/auth/refresh.js
@@ -5,12 +5,11 @@ const { Tokens } = require('../../db/models');
 const key = process.env.secretKey;
 
 async function refresh(req, res) {
-  const { cookies: { refreshToken } } = req;
-  const oldToken = req.headers.authorization.split(' ')[1];
-  const userId = jwt.decode(oldToken).id;
+  const { cookies: { refreshToken }, headers: { authorization: oldToken } } = req;
+  const userId = jwt.decode(oldToken.split(' ')[1]).id;
   try {
     const token = await Tokens.findOne({ where: { token: refreshToken } });
-    if (token) {
+    if (token && token.userId === userId) {
       const newToken = jwt.sign({ id: userId }, key, { expiresIn: 86400 });
       return res.status(200).json({ success: true, token: newToken });
     }
