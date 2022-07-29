dc3645091ed6e19a3d9f0cb98cd3fb0ee2e1922a
iSharovatov1 <i.sharovatov@dunice.net>
2022-07-29 10:13:51 +0300

    token search condition changed

--- controllers/auth/refresh.js
+++ controllers/auth/refresh.js
@@ -8,8 +8,8 @@ async function refresh(req, res) {
   const { cookies: { refreshToken }, headers: { authorization: oldToken } } = req;
   const userId = jwt.decode(oldToken.split(' ')[1]).id;
   try {
-    const token = await Tokens.findOne({ where: { token: refreshToken } });
-    if (token && token.userId === userId) {
+    const token = await Tokens.findOne({ where: { token: refreshToken, userId } });
+    if (token) {
       const newToken = jwt.sign({ id: userId }, key, { expiresIn: 86400 });
       return res.status(200).json({ success: true, token: newToken });
     }
